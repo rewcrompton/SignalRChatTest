68e6fcd63a15b59ccf6149b3007118171d746134
cromptonr <richard.crompton@vizolution.co.uk>
2016-06-15 14:46:57 +0100

    Initial Commit; Empty MVC 4 project with a SignalR hub added.

---
+++ source/SignalRChatTest/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRChatTest
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string message)
+        {
+            Clients.All.hello();
+        }
+    }
+}
