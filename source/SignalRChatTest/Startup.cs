afe46b984ed6396bea0fd60f1a6b13b8c2ffcd73
cromptonr <richard.crompton@vizolution.co.uk>
2016-06-15 14:57:02 +0100

    Added OWIN Startup class to call the MapSignalR method, which creates routes for all SignalR hubs in the application.

---
+++ source/SignalRChatTest/Startup.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRChatTest.Startup))]
+
+namespace SignalRChatTest
+{
+   
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
